054b08e44b3fd26f2ed023af7fbb698791ff438a
yoyomo <armando.josue.og@gmail.com>
2022-12-04 20:45:55 -0800

    top three

--- 2022/day/1/src/main.rs
+++ 2022/day/1/src/main.rs
@@ -1,34 +1,43 @@
 use std::fs;
 
 fn main() {
-    let file_path = "src/input.txt";
+    let contents = fs::read_to_string("src/input.txt").expect("");
 
-    println!("In file {}", file_path);
-
-    let contents = fs::read_to_string(file_path)
-        .expect("Should have been able to read the file");
-
-
-    println!("With text:\n{contents}");
+    println!("{contents}");
 
     let calories: Vec<&str> = contents.split('\n').collect();
 
-    let mut max_num_of_calories: u32 = 0;
+    const TOP_NUM: usize = 3;
+    let mut top_three_calories: [u32; TOP_NUM] = [0,0,0];
+    let mut top_three_total = 0;
+
     let mut num_of_calories: u32 = 0;
     for calorie in calories {
 
         match calorie.parse::<u32>() {
             Ok(c) => num_of_calories += c,
-            Err(_) => num_of_calories = 0,
+            Err(_) => {
+                top_three_total = 0;
+                for (i, top_three) in top_three_calories.iter().enumerate() {
+                    top_three_total += top_three;
+
+                    if num_of_calories > *top_three {
+                        let mut j = TOP_NUM - 1;
+                        while j > i {
+                            top_three_calories[j] = top_three_calories[j-1];
+                            j -= 1;
+                        }
+                        top_three_calories[i] = num_of_calories;
+                        break;
+                    }
+                }
+                num_of_calories = 0;
+            },
         }
 
-        if num_of_calories > max_num_of_calories {
-            max_num_of_calories = num_of_calories;
-        }
-        
     }
 
-    println!("Max num: {max_num_of_calories}")
-
+    println!("Top three: {:?}", top_three_calories);
+    println!("Total num: {top_three_total}")
 
 }
