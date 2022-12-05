912458bb4b665e26c5275613e290087091d43252
yoyomo <armando.josue.og@gmail.com>
2022-12-04 20:45:55 -0800

    day 4 part 1

--- 2022/day/4/src/main.rs
+++ 2022/day/4/src/main.rs
@@ -11,6 +11,10 @@ fn is_fully_contained(a: &Range<u32>, b: &Range<u32>) -> bool{
     a.start >= b.start && a.end <= b.end
 }
 
+fn does_overlap(a: &Range<u32>, b: &Range<u32>) -> bool {
+    a.start >= b.start && a.start <= b.end || a.end <= b.end && a.end >= b.start
+}
+
 fn main() {
     let contents = fs::read_to_string("data/input.txt").expect("");
 
@@ -23,7 +27,7 @@ fn main() {
         let first_range = get_range(pair[0]);
         let second_range = get_range(pair[1]);
 
-        if is_fully_contained(&first_range, &second_range) || is_fully_contained(&second_range, &first_range) {
+        if does_overlap(&first_range, &second_range) || does_overlap(&second_range, &first_range) {
             fully_contained += 1;
         }
 
