3b0385aac206c09cd8ee5e05671309e172410258
armandocumate <armando@documate.org>
2022-12-04 19:41:42 -0800

    day 3 part 2

--- 2022/day/3/src/main.rs
+++ 2022/day/3/src/main.rs
@@ -4,30 +4,34 @@ const BASE_DIFFERENCE: u32 = 64; // ignore all ascii chars before uppercase A
 const UNUSED_DIFFERENCE: u32 = 6; // ascii chars like [] etc
 const TOTAL_LETTERS: u32 = 26;
 
+const GROUP_CAPACITY: u32 = 3;
+
+fn get_priority(item: char) -> u32 {
+    if item.is_ascii_uppercase() {
+        item as u32 - BASE_DIFFERENCE + TOTAL_LETTERS
+    } else {
+        item as u32 - BASE_DIFFERENCE - TOTAL_LETTERS - UNUSED_DIFFERENCE
+    }
+}
+
 fn main() {
     let contents = fs::read_to_string("data/input.txt").expect("");
 
     let rucksacks: Vec<&str> = contents.split('\n').collect();
+
     let mut sum: u32 = 0;
-    for rucksack in rucksacks {
-        let (first_compartment, second_compartment) = rucksack.split_at(rucksack.len() / 2);
+    let mut group_item: char = '\0';
 
-        let mut existing_items: String = "".to_string();
+    for group_rucksacks in rucksacks.chunks(GROUP_CAPACITY as usize) {
 
-        for item in second_compartment.chars() {
-            if first_compartment.contains(item) && !existing_items.contains(item) {
-
-                if item.is_ascii_uppercase() {
-                    sum += item as u32 - BASE_DIFFERENCE + TOTAL_LETTERS;
-                } else {
-                    sum += item as u32 - BASE_DIFFERENCE - TOTAL_LETTERS - UNUSED_DIFFERENCE;
-                }
-
-                existing_items.push(item);
-
+        for item in group_rucksacks[0].chars() {
+            if group_rucksacks[1].contains(item) && group_rucksacks[2].contains(item) {
+                group_item = item;
+                break;
             }
         }
 
+        sum += get_priority(group_item);
 
     }
 
