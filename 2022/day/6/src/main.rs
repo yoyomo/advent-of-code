62ed0eb6df99c9502284b0bca2a50ae4b018e0df
yoyomo <armando.josue.og@gmail.com>
2022-12-05 22:10:26 -0800

    day 6 part1

---
+++ 2022/day/6/src/main.rs
@@ -0,0 +1,22 @@
+use std::{collections::VecDeque, fs};
+
+fn main() {
+    let packet = fs::read_to_string("data/input.txt").expect("");
+
+    const START_OF_PACKET_SIZE: usize = 4;
+    let mut start_of_packet: VecDeque<char> = VecDeque::with_capacity(START_OF_PACKET_SIZE);
+    for (i, character) in packet.char_indices() {
+        if start_of_packet.len() == START_OF_PACKET_SIZE {
+            start_of_packet.pop_front();
+        }
+
+        start_of_packet.push_back(character);
+
+        if start_of_packet.len() == START_OF_PACKET_SIZE {
+            if !start_of_packet.iter().any(|&c| start_of_packet.iter().filter(|&z| c == *z ).count() > 1){
+                println!("start at {} with {:?}", i+1, start_of_packet);
+                break;
+            }
+        }
+    }
+}
