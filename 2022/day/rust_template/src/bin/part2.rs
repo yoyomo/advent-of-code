80ee074fdda0ae1a1833dc86caaf4967dc4eca7e
yoyomo <armando.josue.og@gmail.com>
2022-12-13 23:49:56 -0800

    update rust template to include regex

--- 2022/day/rust_template/src/bin/part2.rs
+++ 2022/day/rust_template/src/bin/part2.rs
@@ -1,7 +1,14 @@
 use std::fs;
 
+use regex::Regex;
+
 fn main() {
     let contents = fs::read_to_string("data/input.txt").expect("");
 
-    let _: Vec<&str> = contents.split('\n').collect();
+    let lines: Vec<&str> = contents.split('\n').collect();
+
+    for line in lines {
+        let re = Regex::new(r"move ([0-9]+) from ([0-9]+) to ([0-9]+)").unwrap();
+        let _ = re.captures(line).unwrap();
+    }
 }
