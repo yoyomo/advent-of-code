e58165d5a3845af0dd4b7dfad5e0ebc8d32937f3
yoyomo <armando.josue.og@gmail.com>
2022-12-15 00:05:55 -0800

    cleanup

--- 2022/day/11/part1/part1.go
+++ 2022/day/11/part1/part1.go
@@ -9,62 +9,75 @@ import (
 )
 
 type Monkey struct {
-	items           []int
-	inspected_count int
+	items           []uint64
+	operation       string
+	op_value        string
+	divisible       uint64
+	if_true         uint64
+	if_false        uint64
+	inspected_count uint64
 }
 
-func parse_items(str string) []int {
+func parse_items(str string) []uint64 {
 	str_items := strings.Split(string(str), ", ")
-	items := []int{}
+	items := []uint64{}
 	for _, str_item := range str_items {
-		item, _ := strconv.Atoi(str_item)
+		item, _ := strconv.ParseUint(str_item, 10, 64)
 		items = append(items, item)
 	}
 	return items
 }
 
-func go_for_rounds(blocks []string, monkeys []Monkey) {
-	for round := 0; round < 20; round++ {
-		for _, block := range blocks {
-			groups := regexp.MustCompile(`Monkey\s(\d+):\n\s+Starting items:\s([\d,\s]+)\n\s+Operation:\snew\s=\sold\s([+*])\s(old|\d+)\n\s+Test:\sdivisible by\s(\d+)\n\s+If true: throw to monkey\s(\d+)\n\s+If false: throw to monkey (\d+)`).FindStringSubmatch(block)
+func init_monkeys(blocks []string, monkeys []Monkey) {
+	for _, block := range blocks {
+		groups := regexp.MustCompile(`Monkey\s(\d+):\n\s+Starting items:\s([\d,\s]+)\n\s+Operation:\snew\s=\sold\s([+*])\s(old|\d+)\n\s+Test:\sdivisible by\s(\d+)\n\s+If true: throw to monkey\s(\d+)\n\s+If false: throw to monkey (\d+)`).FindStringSubmatch(block)
 
-			monkey_index, _ := strconv.Atoi(groups[1])
-			items := parse_items(groups[2])
-			operation := groups[3]
-			op_value := groups[4]
-			divisible, _ := strconv.Atoi(groups[5])
-			if_true, _ := strconv.Atoi(groups[6])
-			if_false, _ := strconv.Atoi(groups[7])
+		monkey_index, _ := strconv.ParseUint(groups[1], 10, 64)
+		monkeys[monkey_index].items = append(parse_items(groups[2]), monkeys[monkey_index].items...)
+		monkeys[monkey_index].operation = groups[3]
+		monkeys[monkey_index].op_value = groups[4]
+		monkeys[monkey_index].divisible, _ = strconv.ParseUint(groups[5], 10, 64)
+		monkeys[monkey_index].if_true, _ = strconv.ParseUint(groups[6], 10, 64)
+		monkeys[monkey_index].if_false, _ = strconv.ParseUint(groups[7], 10, 64)
+	}
+}
 
-			monkey := monkeys[monkey_index]
-
-			if round == 0 {
-				monkey.items = append(items, monkey.items...)
-			}
+func go_for_rounds(monkeys []Monkey, divide_by_three bool) {
+	var MAX_ROUNDS uint64
+	if divide_by_three {
+		MAX_ROUNDS = 20
+	} else {
+		// MAX_ROUNDS = 10000
+		MAX_ROUNDS = 20
+	}
+	for round := uint64(0); round < MAX_ROUNDS; round++ {
+		for monkey_index, monkey := range monkeys {
 
 			for _, item := range monkey.items {
 				monkey.inspected_count++
-				var op_number int
-				switch op_value {
+				var op_number uint64
+				switch monkey.op_value {
 				case "old":
 					op_number = item
 				default:
-					op_number, _ = strconv.Atoi(op_value)
+					op_number, _ = strconv.ParseUint(monkey.op_value, 10, 64)
 				}
 				worry_level := item
-				switch operation {
+				switch monkey.operation {
 				case "*":
 					worry_level *= op_number
 				case "+":
 					worry_level += op_number
 				}
-				worry_level /= 3 //check if need to round
+				if divide_by_three {
+					worry_level /= 3
+				}
 
-				var next_monkey_index int
-				if worry_level%divisible == 0 {
-					next_monkey_index = if_true
+				var next_monkey_index uint64
+				if worry_level%monkey.divisible == 0 {
+					next_monkey_index = monkey.if_true
 				} else {
-					next_monkey_index = if_false
+					next_monkey_index = monkey.if_false
 				}
 
 				monkeys[next_monkey_index].items = append(monkeys[next_monkey_index].items, worry_level)
@@ -77,8 +90,8 @@ func go_for_rounds(blocks []string, monkeys []Monkey) {
 	}
 }
 
-func get_top_two(monkeys []Monkey) [2]int {
-	top_two := [2]int{}
+func get_top_two(monkeys []Monkey) [2]uint64 {
+	top_two := [2]uint64{}
 	for _, monkey := range monkeys {
 		if monkey.inspected_count > top_two[0] {
 			top_two[1] = top_two[0]
@@ -88,8 +101,22 @@ func get_top_two(monkeys []Monkey) [2]int {
 	return top_two
 }
 
-func count_level_of_monkey_business(top_two [2]int) int {
+func count_level_of_monkey_business(top_two [2]uint64) uint64 {
 	return top_two[0] * top_two[1]
+}
+
+func part(blocks []string, divide_by_three bool) {
+	monkeys := make([]Monkey, len(blocks))
+
+	init_monkeys(blocks, monkeys)
+
+	go_for_rounds(monkeys, divide_by_three)
+
+	fmt.Println(monkeys)
+
+	top_two := get_top_two(monkeys)
+	fmt.Println(top_two)
+	fmt.Println(count_level_of_monkey_business(top_two))
 }
 
 func main() {
@@ -97,14 +124,7 @@ func main() {
 
 	blocks := strings.Split(string(dat), "\n\n")
 
-	monkeys := make([]Monkey, len(blocks))
-
-	go_for_rounds(blocks, monkeys)
-
-	fmt.Println(monkeys)
-
-	top_two := get_top_two(monkeys)
-	fmt.Println(top_two)
-	fmt.Println(count_level_of_monkey_business(top_two))
+	part(blocks, true)
+	part(blocks, false)
 
 }
