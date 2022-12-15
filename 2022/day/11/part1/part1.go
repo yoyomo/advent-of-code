fa330e008437be2bba7a734c60081d6faf41618a
yoyomo <armando.josue.og@gmail.com>
2022-12-14 22:54:06 -0800

    write all functions

--- 2022/day/11/part1/part1.go
+++ 2022/day/11/part1/part1.go
@@ -23,60 +23,78 @@ func parse_items(str string) []int {
 	return items
 }
 
-func main() {
-	dat, _ := os.ReadFile("data/input.txt")
+func go_for_rounds(blocks []string, monkeys []Monkey) {
+	for round := 0; round < 1; round++ {
+		for _, block := range blocks {
+			groups := regexp.MustCompile(`Monkey\s(\d+):\n\s+Starting items:\s([\d,\s]+)\n\s+Operation:\snew\s=\sold\s([+*])\s(old|\d+)\n\s+Test:\sdivisible by\s(\d+)\n\s+If true: throw to monkey\s(\d+)\n\s+If false: throw to monkey (\d+)`).FindStringSubmatch(block)
 
-	blocks := strings.Split(string(dat), "\n\n")
+			monkey_index, _ := strconv.Atoi(groups[1])
+			items := parse_items(groups[2])
+			operation := groups[3]
+			op_number, _ := strconv.Atoi(groups[4])
+			divisible, _ := strconv.Atoi(groups[5])
+			if_true, _ := strconv.Atoi(groups[6])
+			if_false, _ := strconv.Atoi(groups[7])
 
-	round := 0
-	monkeys := make(map[int]Monkey)
-	for _, block := range blocks {
-		groups := regexp.MustCompile(`Monkey\s(\d+):\n\s+Starting items:\s([\d,\s]+)\n\s+Operation:\snew\s=\sold\s([+*])\s(old|\d+)\n\s+Test:\sdivisible by\s(\d+)\n\s+If true: throw to monkey\s(\d+)\n\s+If false: throw to monkey (\d+)`).FindStringSubmatch(block)
+			monkey := monkeys[monkey_index]
 
-		monkey_index, _ := strconv.Atoi(groups[1])
-		items := parse_items(groups[2])
-		operation := groups[3]
-		op_number, _ := strconv.Atoi(groups[4])
-		divisible, _ := strconv.Atoi(groups[5])
-		if_true, _ := strconv.Atoi(groups[6])
-		if_false, _ := strconv.Atoi(groups[7])
+			monkey.items = append(items, monkey.items...)
 
-		monkey := monkeys[monkey_index]
+			for _, item := range monkey.items {
+				monkey.inspected_count++
+				worry_level := item
+				switch operation {
+				case "*":
+					worry_level *= op_number
+				case "+":
+					worry_level += op_number
+				}
+				worry_level /= 3 //check if need to round
 
-		monkey.items = append(items, monkey.items...)
-
-		if monkey_index == 0 {
-			round++
-		}
-
-		for _, item := range monkey.items {
-			monkey.inspected_count++
-			worry_level := item
-			switch operation {
-			case "*":
-				worry_level *= op_number
-			case "+":
-				worry_level += op_number
+				var next_monkey_index int
+				if worry_level%divisible == 0 {
+					next_monkey_index = if_true
+				} else {
+					next_monkey_index = if_false
+				}
+				monkeys[next_monkey_index].items = append(monkeys[next_monkey_index].items, worry_level)
 			}
-			worry_level /= 3 //check if need to round
+			monkey.items = []int{}
 
-			var next_monkey Monkey
-			if worry_level%divisible == 0 {
-				next_monkey = monkeys[if_true]
-
-			} else {
-				next_monkey = monkeys[if_false]
-
-			}
-			next_monkey.items = append(next_monkey.items, worry_level)
-		}
-
-		if round == 20 {
-			break
+			monkeys[monkey_index] = monkey
 		}
 
 	}
+}
+
+func get_top_two(monkeys []Monkey) [2]int {
+	top_two := [2]int{}
+	for _, monkey := range monkeys {
+		if monkey.inspected_count > top_two[0] {
+			top_two[1] = top_two[0]
+			top_two[0] = monkey.inspected_count
+		}
+	}
+	return top_two
+}
+
+func count_level_of_monkey_business(top_two [2]int) int {
+	return top_two[0] * top_two[1]
+}
+
+func main() {
+	dat, _ := os.ReadFile("data/example.txt")
+
+	blocks := strings.Split(string(dat), "\n\n")
+
+	monkeys := make([]Monkey, len(blocks))
+
+	go_for_rounds(blocks, monkeys)
 
 	fmt.Println(monkeys)
+
+	top_two := get_top_two(monkeys)
+	fmt.Println(top_two)
+	fmt.Println(count_level_of_monkey_business(top_two))
 
 }
