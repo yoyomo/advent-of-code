406ede847e95fa7706c06424882d6a37bbc19cef
Armando Ortiz <armando.josue.og@gmail.com>
2023-12-07 20:56:12 -0800

    time elapsed

--- 2023/06/src/main.rs
+++ 2023/06/src/main.rs
@@ -2,15 +2,18 @@ mod part2;
 mod part1;
 
 use std::fs::read_to_string;
+use std::time::Instant;
 use crate::part1::part1;
 use crate::part2::part2;
 
 fn main() {
-    let filename = "data/input.txt";
+    let filename = "data/sample.txt";
 
     let binding = read_to_string(filename).expect("");
 
-    println!("{}", part1(binding.split("\n").collect()));
-    println!("{}", part2(binding.split("\n").collect()));
+    let mut start = Instant::now();
+    println!("{} at {:?}", part1(binding.split("\n").collect()), start.elapsed());
+    start = Instant::now();
+    println!("{} at {:?}", part2(binding.split("\n").collect()), start.elapsed());
     return
 }
