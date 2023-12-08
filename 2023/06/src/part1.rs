e6f37f91fbce44aad87d93becc59c80b8a1e8be2
Armando Ortiz <armando.josue.og@gmail.com>
2023-12-07 20:56:12 -0800

    day 06 part2

--- 2023/06/src/part1.rs
+++ 2023/06/src/part1.rs
@@ -1,7 +1,8 @@
 use regex::{Captures, Regex};
+use crate::part2::get_wins;
 
-fn get_num_array(g: Captures) -> Vec<u32> {
-    let mut num_array: Vec<u32> = vec![];
+fn get_num_array(g: Captures) -> Vec<u64> {
+    let mut num_array: Vec<u64> = vec![];
     let times_str = g.get(1).unwrap().as_str().split(" ");
     for time_str in times_str {
         match time_str.parse() {
@@ -17,8 +18,8 @@ pub fn part1(lines: Vec<&str>) -> i32 {
     let time_re = Regex::new(r"Time:(.*)").unwrap();
     let distance_re = Regex::new(r"Distance:(.*)").unwrap();
 
-    let mut times: Vec<u32> = vec![];
-    let mut distances: Vec<u32> = vec![];
+    let mut times: Vec<u64> = vec![];
+    let mut distances: Vec<u64> = vec![];
 
     for line in lines {
         match time_re.captures(line) {
@@ -46,22 +47,7 @@ pub fn part1(lines: Vec<&str>) -> i32 {
         let time = times[race];
         let distance = distances[race];
 
-        let mut hold_the_button = 0;
-        let mut wins = 0;
-        while hold_the_button < times[race] {
-
-            let distance_traveled = (time - hold_the_button) * hold_the_button;
-
-            if distance_traveled > distance {
-                wins += 1;
-            } else if wins > 0 {
-                break;
-            }
-
-            hold_the_button += 1;
-        }
-
-        result *= wins;
+        result *= get_wins(time, distance);
     }
 
     return result
--- 2023/06/src/part2.rs
+++ 2023/06/src/part2.rs
@@ -1,8 +1,49 @@
-pub fn part2(lines: Vec<&str>) -> &'static str {
+use regex::{Captures, Regex};
 
-    for _ in lines {
+fn get_number(g: Captures) -> u64 {
+    return g.get(1).unwrap().as_str().split_whitespace().collect::<String>().parse().unwrap();
+}
 
+pub fn get_wins(time: u64, distance: u64) -> i32 {
+    let mut hold_the_button = 0;
+    let mut wins = 0;
+    while hold_the_button < time {
+        let distance_traveled = (time - hold_the_button) * hold_the_button;
+
+        if distance_traveled > distance {
+            wins += 1;
+        } else if wins > 0 {
+            break;
+        }
+
+        hold_the_button += 1;
+    }
+    return wins
+}
+
+pub fn part2(lines: Vec<&str>) -> i32 {
+    let time_re = Regex::new(r"Time:(.*)").unwrap();
+    let distance_re = Regex::new(r"Distance:(.*)").unwrap();
+
+    let mut time = 0;
+    let mut distance = 0;
+
+    for line in lines {
+        match time_re.captures(line) {
+            Some(g) => {
+                time = get_number(g)
+            }
+            None => {}
+        }
+        match distance_re.captures(line) {
+            Some(g) => {
+                distance = get_number(g)
+            }
+            None => {}
+        }
     }
 
-    ""
+
+
+    return get_wins(time, distance);
 }
