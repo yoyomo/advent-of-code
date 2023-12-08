e309fc4a750f550e42597ecdaf7f0312f20c6d6e
Armando Ortiz <armando.josue.og@gmail.com>
2023-12-07 20:56:12 -0800

    rust_template with regex

--- 2023/rust_template/src/part1.rs
+++ 2023/rust_template/src/part1.rs
@@ -1,9 +1,13 @@
 use std::str::Lines;
+use regex::Regex;
 
 pub fn part1(lines: Lines) -> &'static str {
 
+    let re = Regex::new(r".*").unwrap();
+
     for line in lines {
-        println!("{}", line)
+        let groups = re.captures(line).unwrap();
+        println!("{}", groups.get(0).unwrap().as_str())
     }
 
     return ""
