c2b6034402fac97beac783996b0bbd57dbbcbcab
Armando Ortiz <armando.josue.og@gmail.com>
2023-12-07 22:15:57 -0800

    part 1

--- 2023/08/src/part1.rs
+++ 2023/08/src/part1.rs
@@ -2,53 +2,58 @@ use std::collections::HashMap;
 use regex::{Regex};
 
 pub struct Node {
-    left: &'static str,
-    right: &'static str,
+    left: String,
+    right: String,
 }
 
 pub fn part1(lines: Vec<&str>) -> usize {
     let instructions_re = Regex::new(r"([RL]+)").unwrap();
     let node_re = Regex::new(r"([A-Z]{3})\s=\s\(([A-Z]{3}),\s([A-Z]{3})\)").unwrap();
 
-    let mut instructions = "";
+    let mut instructions: String = "".to_string();
 
-    let mut nodes: HashMap<&str, Node> = HashMap::new();
+    let mut nodes: HashMap<String, Node> = HashMap::new();
+    let mut first_node: String = "".to_string();
 
     for line in lines {
-        match instructions_re.captures(line) {
-            Some(g) => {
-                instructions = g.get(1).unwrap().as_str();
-                continue;
+        if instructions.is_empty(){
+            match instructions_re.captures(line) {
+                Some(g) => {
+                    instructions = g.get(1).unwrap().as_str().to_string();
+                }
+                None => {}
             }
-            None => {}
         }
 
         match node_re.captures(line) {
             Some(g) => {
-                let node = g.get(1).unwrap().as_str();
-                let left = g.get(2).unwrap().as_str();
-                let right = g.get(3).unwrap().as_str();
+                let node = g.get(1).unwrap().as_str().to_string();
+                let left = g.get(2).unwrap().as_str().to_string();
+                let right = g.get(3).unwrap().as_str().to_string();
+                if first_node.is_empty() {
+                    first_node = node.clone();
+                }
                 nodes.insert(node, Node {
                     left,
                     right,
                 });
+
             }
             None => {}
         }
     }
 
-    let mut current_node = "AAA";
+    let mut current_node = "AAA".to_string();
     let mut steps: usize = 1;
     let mut i: usize = 0;
-    while i < i % instructions.len() {
+    loop {
         let instruction = instructions.chars().nth(i).unwrap();
-
         match instruction {
             'L' => {
-                current_node = nodes.get(current_node).unwrap().left
+                current_node = nodes.get(current_node.as_str()).unwrap().left.clone()
             }
             'R' => {
-                current_node = nodes.get(current_node).unwrap().right
+                current_node = nodes.get(current_node.as_str()).unwrap().right.clone()
             }
             _ => {}
         }
